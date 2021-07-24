f0371f8ac89c157b7655ab05f84a2066783c7ef5
Kushagra2403 <singhkushagra2403@gmail.com>
2021-07-24 19:01:29 +0530

    Register and Login User Completed

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require("express");
+const connectDb = require("./config/db");
 const app = express();
 
+// Connect to DB
+connectDb();
+
 app.get("/", (req, res) => res.send("API Running"));
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+// Define Routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/posts", require("./routes/api/posts"));
+app.use("/api/profile", require("./routes/api/profile"));
 
 const PORT = process.env.PORT || 5000;
 
